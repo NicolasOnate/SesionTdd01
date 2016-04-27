8614b7297a584d1386fded1a78f1faf283b794a3
NicolasOnate <nionate@alumnos.ubiobio.cl>
2016-04-27 12:00:00 -0400

    2

--- src/main/java/cl/ubb/agil/Calculadora.java
+++ src/main/java/cl/ubb/agil/Calculadora.java
@@ -1,5 +1,13 @@
 package cl.ubb.agil;
 
 public class Calculadora {
+	
+	public Calculadora(){
+		
+	}
+
+	public int sumar(int i, int j) {
+		return 0;
+	}
 
 }
--- src/test/java/cl/ubb/agil/CalculadoraTest.java
+++ src/test/java/cl/ubb/agil/CalculadoraTest.java
@@ -1,14 +1,20 @@
 package cl.ubb.agil;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.Test;
 
 public class CalculadoraTest {
-
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void sumarDosMasTresEsCinco() {
+		Calculadora  calculadora = new Calculadora();
+		
+		int resultado = calculadora.sumar(2, 3);
+		
+		assertThat(resultado,  is(5));
 	}
 
 }
