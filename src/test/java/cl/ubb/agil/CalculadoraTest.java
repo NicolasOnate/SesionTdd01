552d486a6af0b4a24a4a008934591f2565e40fa0
NicolasOnate <nionate@alumnos.ubiobio.cl>
2016-04-27 11:37:13 -0400

    primer commit

---
+++ src/test/java/cl/ubb/agil/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package cl.ubb.agil;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
